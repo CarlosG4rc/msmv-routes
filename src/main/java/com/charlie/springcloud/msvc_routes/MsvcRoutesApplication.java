2d43be991930e62698f5064ce03e59cf75e6fbf2
Carlos García <67180099+CarlosG4rc@users.noreply.github.com>
2025-09-26 14:42:02 -0600

    First commit

---
+++ src/main/java/com/charlie/springcloud/msvc_routes/MsvcRoutesApplication.java
@@ -0,0 +1,12 @@
+package com.charlie.springcloud.msvc_routes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcRoutesApplication.class, args);
+	}
+}
---
+++ src/main/java/com/charlie/springcloud/msvc_routes/controller/JarController.java
@@ -0,0 +1,35 @@
+package com.charlie.springcloud.msvc_routes.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.jar.JarFile;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/jar")
+public class JarController {
+    @PostMapping("/analyze")
+    public List<String> AnalyzeJar(@RequestParam("file") MultipartFile file) throws IOException{
+    	List<String> filePaths = new ArrayList<>();
+    	
+    	//Se guarda temporalmente el archivo
+    	File tempFile = File.createTempFile("upload-",  ".jar");
+    	file.transferTo(tempFile);
+    	
+    	//Lee el jar
+    	try(JarFile jarFile = new JarFile(tempFile)) {
+    		jarFile.stream().forEach(entry -> filePaths.add(entry.getName()));
+    	}
+    	//Borramos archivo
+    	tempFile.delete();
+    	
+    	return filePaths;
+    }
+}
